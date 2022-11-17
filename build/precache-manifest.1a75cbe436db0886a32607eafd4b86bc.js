3023d8eb37651ebac14ca13008441b96f8647c55
Freewhisper77 <emmasddx@foxmail.com>
2022-11-17 19:49:43 +0800

    first commit

---
+++ build/precache-manifest.1a75cbe436db0886a32607eafd4b86bc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654ba2e0d16f6516f02143a11cbe1039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afe6e63d5954b890835b",
+    "url": "/static/js/2.502d6c76.chunk.js"
+  },
+  {
+    "revision": "374bdbe9d508942b5bf6",
+    "url": "/static/js/main.d7a179dc.chunk.js"
+  },
+  {
+    "revision": "20e0c32b7f4b3a4ff918",
+    "url": "/static/js/runtime-main.a92850ae.js"
+  }
+]);
