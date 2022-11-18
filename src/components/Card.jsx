9acf907d3980cc93e2dcf3df7dc407d969e7e691
Freewhisper77 <emmasddx@foxmail.com>
2022-11-18 10:08:03 +0800

    feat:  create a Avatar component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Avatar from "./Avatar";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt={props.alt} />
+        <Avatar img={props.img} alt={props.alt} />
+        {/*如何将该图像属性传递给新建的Avatar组件 */}
       </div>
       <div className="bottom">
         <p className="info">{props.tel}</p>
