b2c15529fb4d62ec1eb519ffa6bf382e0b803760
Freewhisper77 <emmasddx@foxmail.com>
2022-11-18 08:47:53 +0800

    style: add css styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,12 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
-        <h2>{props.name}</h2>
-        <img src={props.img} alt={props.alt} />
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.img} alt={props.alt} />
       </div>
       <div className="bottom">
-        <p>{props.tel}</p>
-        <p>{props.email}</p>
+        <p className="info">{props.tel}</p>
+        <p className="info">{props.email}</p>
       </div>
     </div>
   );
