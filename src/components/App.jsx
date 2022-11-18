84532a0b00b12061203ac0dd27a6e5c56b0a1b24
Freewhisper77 <emmasddx@foxmail.com>
2022-11-18 10:49:21 +0800

    feat:  directly use Avatar in App(just use Avatar in Card before)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 const imgAlt = "avatar_img";
 
@@ -8,6 +9,11 @@ function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar
+        img={
+          "https://media.gettyimages.com/id/1170383972/photo/taylor-swift-attends-the-2019-mtv-video-music-awards-at-prudential-center-on-august-26-2019.jpg?s=612x612&w=gi&k=20&c=5gtR_5WjtnQPWM6RDELGRlDlycdHz7h67ABJYHPaATQ="
+        }
+      />
       <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
